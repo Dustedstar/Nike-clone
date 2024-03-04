daeb9bed0e3fef568962ef64e2f1a157f53a66fe
Mehryar <gargoyle.junior@gmail.com>
2024-03-04 23:21:07 +0330

    First Commit
    Date: 03/04/2024

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Footer from "./Footer";
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import PopularProducts from "./PopularProducts";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import SpecialOffer from "./SpecialOffer";
+import Services from "./Services";
+
+export {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Subscribe,
+  SuperQuality,
+  SpecialOffer,
+  Services,
+};
